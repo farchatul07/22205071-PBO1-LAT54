f76be352e835e897d4eca50c2c6e61cc6fcd616f
farchatul07 <145920038+farchatul07@users.noreply.github.com>
2023-11-29 15:42:16 +0700

    Add files via upload

---
+++ src/pkg22205071_pbo_lat54/Koordinat.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkg22205071_pbo_lat54;
+
+/**
+ *
+ * @author user
+ */
+public class Koordinat {
+    protected int x;
+    protected int y;
+
+    public Koordinat(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+}
+
+class WarnaKoordinat extends Koordinat {
+
+    public String namaWarna;
+
+    public WarnaKoordinat(int x, int y, String namaWarna) {
+        super(x, y);
+        this.x = x;
+        this.y = y;
+        this.namaWarna = namaWarna;
+    }
+
+    public String getNamaWarna() {
+        return namaWarna;
+    }
+
+    public void setNamaWarna(String namaWarna) {
+        this.namaWarna = namaWarna;
+    }
+}
+
+class KoordinatTester{
+    
+    public static void main(String[] args) {
+        WarnaKoordinat  wk = new WarnaKoordinat(10, 4, "Jingga");
+        System.out.println("Warna Koordinat : " + wk.getNamaWarna());
+        System.out.println("Koordinat Sumbu x : " + wk.getX() + ", y : " + wk.getY());
+    }
+}
